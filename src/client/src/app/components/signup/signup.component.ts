65a1ca22dc6d7ea8e81d7c77a703c604de3d69e2
Sirisha <svoggu@alphaworks.tech>
2022-03-21 12:10:45 -0700

    feat(client): signup page setup

---
+++ src/client/src/app/components/signup/signup.component.ts
@@ -0,0 +1,30 @@
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+
+@Component({
+  selector: 'app-signup',
+  templateUrl: './signup.component.html',
+  styleUrls: ['./signup.component.scss']
+})
+export class SignupComponent implements OnInit {
+  addUser: FormGroup;
+  constructor(
+    private fb: FormBuilder,
+  ) { 
+    this.addUser = this.fb.group({
+      username: ['', Validators.required],
+      age: [
+        '',
+        Validators.compose([Validators.required, Validators.minLength(3)]),
+      ],
+      password: [
+        '',
+        Validators.compose([Validators.required, Validators.minLength(5)]),
+      ],
+    });
+  }
+
+  ngOnInit(): void {
+  }
+
+}
