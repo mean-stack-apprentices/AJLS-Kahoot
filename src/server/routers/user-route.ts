a9ae96d9772b7dc3deae104ae387dcb87bbb3f53
Lee <tab271621@gmail.com>
2022-03-25 17:35:13 -0500

    feat(server)add login api inside userRouter

--- src/server/routers/user-route.ts
+++ src/server/routers/user-route.ts
@@ -20,3 +20,30 @@ userRouter.post("/create-user", function (req, res) {
         res.json({ errors: err });
       });
   });
+
+  userRouter.get("/login",function(req,res){
+    const username = req.body.username
+    const password = req.body.password
+    UserModel
+    .findOne({ username })
+    .then(user => {
+      if(!user) {
+        res.json("Invalid username");
+        return;
+      }
+      else {
+        if(user?.password == password) {
+          res.json({ data: user });
+        }
+        else {
+          res.json("Invalid password");
+        }
+      }
+    })
+    .catch((err) => {
+      res.status(501);
+      res.json({ errors: err });
+    });
+   });
+
+  
