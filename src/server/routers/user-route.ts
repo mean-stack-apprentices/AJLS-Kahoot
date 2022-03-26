082e11e63c95514964df3622c4657f5e0573b7ef
Lee <tab271621@gmail.com>
2022-03-26 15:25:06 -0500

    feat(server)add jwt and assign cookie in login api

--- src/server/routers/user-route.ts
+++ src/server/routers/user-route.ts
@@ -1,7 +1,11 @@
 import express  from "express";
 import { UserModel } from "../schemas/user.schema.js";
+import jwt from "jsonwebtoken";
+import dotenv from "dotenv";
 
+dotenv.config();
 export const userRouter = express.Router()
+const access_secret = process.env.ACCESS_TOKEN_SECRET as string;
 
 userRouter.post("/create-user", function (req, res) {
     const {username, password, age} = req.body;
@@ -33,7 +37,11 @@ userRouter.post("/create-user", function (req, res) {
       }
       else {
         if(user?.password == password) {
-          res.json(user);
+          const accessToken = jwt.sign({ user }, access_secret);
+          console.log("Token", accessToken);
+          res.cookie("jwt", accessToken, {httpOnly: true,maxAge: 60 * 60 * 1000});
+          res.json({message: 'Successfully Logged In', user})
+      
         }
         else {
           res.status(401).send("Invalid password");
