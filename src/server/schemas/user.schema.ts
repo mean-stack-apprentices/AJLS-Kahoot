97db965edbbe8807637558f77968d106f704d4af
Aman <amandeepk221@gmail.com>
2022-03-27 16:59:55 -0700

    feat(schemas): add error messages

--- src/server/schemas/user.schema.ts
+++ src/server/schemas/user.schema.ts
@@ -3,9 +3,9 @@ import type { User } from '../../shared/models/user.model.js';
 const {Schema, model} = mongoose
 
 const userSchema = new Schema<User>({
-    username: {type: String, required: true},
-    password: {type: String, required: true},
-    age: {type: Number, required: true},
+    username: {type: String, required: [true, "username is required"]},
+    password: {type: String, required: [true, "password is required"]},
+    age: {type: Number, required: [true, "age is required"]},
 });
 
 export const UserModel = model<User>('User',userSchema);
