7b532ae426d9baf38ad4082743bcfbf48e2b3ea9
Aman <amandeepk221@gmail.com>
2022-03-27 12:33:08 -0700

    feat(client,store): create selector to store signUpFailure reducer

--- src/client/src/app/store/selectors/user/user.selectors.ts
+++ src/client/src/app/store/selectors/user/user.selectors.ts
@@ -1,2 +1,10 @@
 import { createFeatureSelector, createSelector } from '@ngrx/store';
+import { AppState } from '../..';
+import * as fromUser from '../../reducers/user/user.reducer';
 
+const userFeatureSelector = createFeatureSelector<AppState, fromUser.State>(fromUser.userFeatureKey);
+
+export const signUpFailureSelector = createSelector(
+    userFeatureSelector,
+    (state) => state.signUpFailure
+)
