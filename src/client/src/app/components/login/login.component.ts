5eaf1142edd6aaf60f76b5972c1a60008d9f64dd
Lee <tab271621@gmail.com>
2022-03-28 18:08:12 -0500

    feat(client)add login method and loginfailmsg variable

--- src/client/src/app/components/login/login.component.ts
+++ src/client/src/app/components/login/login.component.ts
@@ -1,5 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Store } from '@ngrx/store';
+import { Observable } from 'rxjs';
+import { AuthService } from 'src/app/services/auth.service';
+import { AppState } from 'src/app/store';
+import { loginUser } from 'src/app/store/actions/user/user.actions';
+import { loginFailureSelector } from 'src/app/store/selectors/user/user.selectors';
 
 @Component({
   selector: 'app-login',
@@ -9,12 +15,22 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 export class LoginComponent implements OnInit {
   loginForm: FormGroup;
-    constructor( private fb: FormBuilder) { 
+  loginFailMsg$: Observable<string>;
+    constructor( private fb: FormBuilder, private authService: AuthService, private store: Store<AppState>) {
+
+      this.loginFailMsg$ = this.store.select(loginFailureSelector );
       this.loginForm = this.fb.group({
         username: ['', Validators.required],
         password: ['', Validators.required],
       });
+
+
     }
-    ngOnInit(): void {   
+    ngOnInit(): void {
     }
+
+    login() {
+      this.store.dispatch(loginUser({ data: this.loginForm.value }))
+    }
+
   }
