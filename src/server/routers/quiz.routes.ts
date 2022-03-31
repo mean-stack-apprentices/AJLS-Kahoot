2161151786a81638131d3aeaf9cadfcd62ccfe9a
Aman <amandeepk221@gmail.com>
2022-03-31 16:53:41 -0700

    feat(server, routers): create api call for create-quiz-question

--- src/server/routers/quiz.routes.ts
+++ src/server/routers/quiz.routes.ts
@@ -1,5 +1,7 @@
 import express from "express";
+import { QuestionModel } from "../schemas/question.schema.js";
 import { QuizModel } from "../schemas/quiz.schema.js";
+import * as QuizProcess from "./../middleware/quiz.middleware.js";
 
 export const quizRouter = express.Router();
 
@@ -12,9 +14,16 @@ quizRouter.post("/create-quiz", function (req, res) {
     quiz
     .save()
     .then((data) => {
-        res.json({ message: "Quiz created Successfully", data });
+        res.json({ message: "Quiz title created Successfully", data });
       })
     .catch((err) => {
         res.status(501).json(err);
       });
 });
+
+quizRouter.post('/create-quiz-question', function(req,res){
+  
+  QuizProcess.createQuestionAndUpdateQuiz
+
+});
+
