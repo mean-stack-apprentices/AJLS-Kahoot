f81f57aeae83a03009d7dc0c9259a317166fec32
Aman <amandeepk221@gmail.com>
2022-03-30 20:18:26 -0700

    feat(client): add routes for createquiz component

--- src/client/src/app/app-routing.module.ts
+++ src/client/src/app/app-routing.module.ts
@@ -5,12 +5,14 @@ import { LoginComponent } from './components/login/login.component';
 import { HomeComponent } from './components/home/home.component';
 
 import { SignupComponent } from './components/signup/signup.component';
+import { CreateQuiz1Component } from './components/create-quiz1/create-quiz1.component';
 
 
 const routes: Routes = [
   {path: '', component: HomeComponent},
   {path: 'signup', component: SignupComponent},
-  {path: 'login', component: LoginComponent}
+  {path: 'login', component: LoginComponent},
+  {path: 'create-quiz1', component: CreateQuiz1Component}
 ];
 
 @NgModule({
