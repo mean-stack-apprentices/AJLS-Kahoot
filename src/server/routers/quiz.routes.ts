e6520f04fb769660150f741a4328e5cae83c2ae4
Aman <amandeepk221@gmail.com>
2022-03-31 07:21:46 -0700

    ref(server,routers): pretiffy code

--- src/server/routers/quiz.routes.ts
+++ src/server/routers/quiz.routes.ts
@@ -4,21 +4,17 @@ import { QuizModel } from "../schemas/quiz.schema.js";
 export const quizRouter = express.Router();
 
 quizRouter.post("/create-quiz", function (req, res) {
-    const { title} = req.body;
+    const {title} = req.body;
 
     const quiz = new QuizModel({
         title
       });
-
-
-quiz
+    quiz
     .save()
-        .then((data) => {
-          res.json({ message: "Quiz created Successfully", data });
-        })
-        .catch((err) => {
-          res.status(501);
-          res.json(err);
-        });
-    
-    });
+    .then((data) => {
+        res.json({ message: "Quiz created Successfully", data });
+      })
+    .catch((err) => {
+        res.status(501).json(err);
+      });
+});
