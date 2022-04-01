0c8cfdd18876535424026db395b887426a4e5a4d
Aman <amandeepk221@gmail.com>
2022-03-31 18:54:14 -0700

    feat(client): create ngForm for question

--- src/client/src/app/components/create-quiz-question/create-quiz-question.component.ts
+++ src/client/src/app/components/create-quiz-question/create-quiz-question.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-create-quiz-question',
@@ -6,8 +7,25 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./create-quiz-question.component.scss']
 })
 export class CreateQuizQuestionComponent implements OnInit {
+  createQuestionForm: FormGroup;
+  constructor(
+    private fb: FormBuilder
+  ) 
+  { 
+    this.createQuestionForm = this.fb.group({
+      question_title: ['', Validators.compose([Validators.required, Validators.minLength(5), Validators.maxLength(20)])],
 
-  constructor() { }
+      option1: ['', Validators.compose([Validators.required, Validators.maxLength(20)])],
+
+      option2: ['', Validators.compose([Validators.required, Validators.maxLength(20)])],
+
+      option3: ['', Validators.compose([Validators.required, Validators.maxLength(20)])],
+
+      option4: ['', Validators.compose([Validators.required, Validators.maxLength(20)])],
+
+
+    })
+  }
 
   ngOnInit(): void {
   }
