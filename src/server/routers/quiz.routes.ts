2eb96d93ce3d1a6236227b7b2d290001c7231b7a
Lee <tab271621@gmail.com>
2022-04-04 19:12:33 -0500

    feat(server )create-request to get all quizzes

--- src/server/routers/quiz.routes.ts
+++ src/server/routers/quiz.routes.ts
@@ -1,4 +1,5 @@
 import express from "express";
+import { authHandler } from "../middleware/auth.middleware.js";
 import { QuestionModel } from "../schemas/question.schema.js";
 import { QuizModel } from "../schemas/quiz.schema.js";
 import * as QuizProcess from "./../middleware/quiz.middleware.js";
@@ -6,20 +7,31 @@ import * as QuizProcess from "./../middleware/quiz.middleware.js";
 export const quizRouter = express.Router();
 
 quizRouter.post("/create-quiz-title", function (req, res) {
-    const {title} = req.body;
+	const { title } = req.body;
 
-    const quiz = new QuizModel({
-        title
-      });
-    quiz
-    .save()
-    .then((data) => {
-        res.json({ message: "Quiz title created Successfully", data });
-      })
-    .catch((err) => {
-        res.status(501).json(err);
-      });
+	const quiz = new QuizModel({
+		title,
+	});
+	quiz
+		.save()
+		.then((data) => {
+			res.json({ message: "Quiz title created Successfully", data });
+		})
+		.catch((err) => {
+			res.status(501).json(err);
+		});
 });
 
-quizRouter.post('/create-quiz-question',QuizProcess.createQuestionAndUpdateQuiz);
+quizRouter.post(
+	"/create-quiz-question",
+	QuizProcess.createQuestionAndUpdateQuiz
+);
 
+quizRouter.get("/quizzes", function (req, res) {
+	QuizModel.find()
+		.then((data) => res.json({ data }))
+		.catch((err) => {
+			res.status(501);
+			res.json({ errors: err });
+		});
+});
