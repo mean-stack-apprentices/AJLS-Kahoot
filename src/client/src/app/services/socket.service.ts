ae27f0500c79f3cb38474fefe58748cbdab98e8c
Aman <amandeepk221@gmail.com>
2022-04-05 07:20:52 -0700

    feat(server):  create socket connection for testing

--- src/client/src/app/services/socket.service.ts
+++ src/client/src/app/services/socket.service.ts
@@ -1,9 +1,16 @@
 import { Injectable } from '@angular/core';
+import { Socket } from 'ngx-socket-io';
 
 @Injectable({
   providedIn: 'root'
 })
 export class SocketService {
 
-  constructor() { }
+  constructor(
+    private socket: Socket
+  ) 
+  { 
+    this.socket.on('connect', () => {})
+  }
+
 }
