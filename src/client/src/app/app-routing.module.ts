41ef1393b886ec20b9cb6899c86aba129d9ca676
Aman <amandeepk221@gmail.com>
2022-04-05 19:17:59 -0700

    feat(client): add id as route params in routing module

--- src/client/src/app/app-routing.module.ts
+++ src/client/src/app/app-routing.module.ts
@@ -19,7 +19,7 @@ const routes: Routes = [
   {path: 'quiz-list', component: QuizListComponent},
   {path: 'quiz-details', component: QuizDetailsComponent},
   {path: 'create-quiz-title', component: CreateQuizTitleComponent},
-  {path: 'create-question', component: CreateQuizQuestionComponent},
+  {path: 'create-question/:quizId', component: CreateQuizQuestionComponent},
 ];
 
 @NgModule({
