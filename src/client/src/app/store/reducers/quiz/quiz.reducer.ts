d896edb7099a2fa832890480dda50888c53e6bba
Sirisha <svoggu@alphaworks.tech>
2022-04-11 16:22:22 -0700

    ref(client): remove unwanted lines

--- src/client/src/app/store/reducers/quiz/quiz.reducer.ts
+++ src/client/src/app/store/reducers/quiz/quiz.reducer.ts
@@ -1,28 +1,23 @@
-import { Action, createReducer, on } from '@ngrx/store';
+import { createReducer, on } from '@ngrx/store';
 import { Quiz } from '../../../../../../shared/models/quiz.model';
 import {
   createQuizTitleFailure,
   createQuizTitleSuccess,
-  loadQuizDetailsSuccess,
+  selectQuizSuccess,
   loadQuizzesSuccess,
-  selectQuizAction,
-} from '../../actions/quiz/quiz.actions';
+  } from '../../actions/quiz/quiz.actions';
 
 export const quizFeatureKey = 'quiz';
 
 export interface State {
   quizs: Quiz[];
   selectedQuiz: Quiz | null;
-  // createQuizTitleSuccessMessage: string | null;
-  // createQuizTitleFailMessage: string | null;
-}
+  }
 
 export const initialState: State = {
   quizs: [],
   selectedQuiz: null,
-  // createQuizTitleSuccessMessage: null,
-  // createQuizTitleFailMessage: null,
-};
+  };
 
 export const reducer = createReducer(
   initialState,
@@ -39,20 +34,14 @@ export const reducer = createReducer(
   on(createQuizTitleFailure, (state, action) => {
     return {
       ...state
-      // createQuizSuccessMessage: null,
-      // createQuizFailMessage: action.error.message,
-    };
+         };
   }),
   
   on(loadQuizzesSuccess,(state,action)=>{
     return {...state, quizs:action.data}
   }),
 
-  on(selectQuizAction, (state, action) => {
-    return { ...state, selectedQuiz: action.data }
-  }),
-
-  on(loadQuizDetailsSuccess,(state,action)=>{
-    return {...state, quizs:action.data}
+  on(selectQuizSuccess,(state,action)=>{
+    return {...state, selectedQuiz:action.data}
   })
 );
