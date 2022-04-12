415ff97f04b56ab859b83bcd532c65eccb8134bc
Aman <amandeepk221@gmail.com>
2022-04-12 11:54:47 -0700

    chore(client): ng generate component game-pin

---
+++ src/client/src/app/components/game-pin/game-pin.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-game-pin',
+  templateUrl: './game-pin.component.html',
+  styleUrls: ['./game-pin.component.scss']
+})
+export class GamePinComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
