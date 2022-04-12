ab472bb19f29f8bef93c6badacf4a38458b25930
Aman <amandeepk221@gmail.com>
2022-04-12 13:01:18 -0700

    feat(client): remove join-game and add game-pin

--- src/client/src/app/app-routing.module.ts
+++ src/client/src/app/app-routing.module.ts
@@ -1,23 +1,20 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './components/login/login.component';
-import { JoinGameComponent } from './components/join-game/join-game.component';
-
 import { HomeComponent } from './components/home/home.component';
-
 import { SignupComponent } from './components/signup/signup.component';
 import { CreateQuizQuestionComponent } from './components/create-quiz-question/create-quiz-question.component';
-
 import { QuizListComponent } from './components/quiz-list/quiz-list.component'
 import { CreateQuizTitleComponent } from './components/create-quiz-title/create-quiz-title.component'
 import { QuizDetailsComponent } from './components/quiz-details/quiz-details.component';
+import { GamePinComponent } from './components/game-pin/game-pin.component';
 
 
 const routes: Routes = [
   {path: '', component: HomeComponent},
   {path: 'signup', component: SignupComponent},
   {path: 'login', component: LoginComponent},
-  {path: 'join-game', component: JoinGameComponent},
+  {path: 'game-pin', component: GamePinComponent},
   {path: 'quiz-list', component: QuizListComponent},
   {path: 'quiz-details/:quizId', component: QuizDetailsComponent},
   {path: 'create-quiz-title', component: CreateQuizTitleComponent},
