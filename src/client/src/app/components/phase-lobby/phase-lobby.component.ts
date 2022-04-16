c036c736f786daa9053f9e5d7c2494b85d42a363
Lee <tab271621@gmail.com>
2022-04-16 00:32:03 -0500

    feat(client ): create phase-lobby component and add Button

---
+++ src/client/src/app/components/phase-lobby/phase-lobby.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-phase-lobby',
+  templateUrl: './phase-lobby.component.html',
+  styleUrls: ['./phase-lobby.component.scss']
+})
+export class PhaseLobbyComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
