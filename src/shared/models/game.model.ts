a422af614e8dc9d05543e113377df5dc75044f41
Aman <amandeepk221@gmail.com>
2022-04-28 15:13:05 -0700

    ref(shared): add host property in Game

--- src/shared/models/game.model.ts
+++ src/shared/models/game.model.ts
@@ -1,9 +1,15 @@
 import { Player } from "./player.model.js";
 import { Quiz } from "./quiz.model.js";
 
+export interface Host {
+    socketId: string;
+    hostName?: string;
+}
+
 export interface Game{
     phases: string[];
+    host: Host | null;
     players: Player[];
     quiz: Quiz | null;
-    gamePin: string | null;
+    gamePin: string | null; 
 }
