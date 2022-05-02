2983a166f32a1d41c6ce694a0e8fb2274a6e61ed
Aman <amandeepk221@gmail.com>
2022-05-02 13:18:59 -0700

    ref(client): remove unused imports

--- src/client/src/app/components/game-pin/game-pin.component.ts
+++ src/client/src/app/components/game-pin/game-pin.component.ts
@@ -1,5 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import { Socket } from 'ngx-socket-io';
 import { Observable } from 'rxjs';
 import { SocketService } from 'src/app/services/socket.service';
 
